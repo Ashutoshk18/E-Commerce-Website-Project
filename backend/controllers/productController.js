60aa0927bf3ebbd107aa3b4df69848982fc8e318
Ashutoshk18 <ashutoshku1803@gmail.com>
2025-12-15 22:56:54 +0530

    Setup backend and update frontend, add root gitignore

---
+++ backend/controllers/productController.js
@@ -0,0 +1,13 @@
+// Function for add product
+const addProduct = async (req, res) => {};
+
+//Function for list product
+const listProducts = async (req, res) => {};
+
+// Function for removing product
+const removeProduct = async (req, res) => {};
+
+// Function ofr single product info
+const singleProduct = async (req, res) => {};
+
+export { listProducts, addProduct, removeProduct, singleProduct };
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,27 @@
 import express from "express";
 import cors from "cors";
-import 'dotenv/config';
+import "dotenv/config";
 import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
 
 // App config
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
-// Connect to database (non-blocking - server will run even if DB fails)
-connectDB().catch(err => {
-    console.error("Database connection error:", err.message);
-    console.log("Server will continue running without database connection");
-});
-
-// middle wares
+// Middlewares
 app.use(express.json());
 app.use(cors());
 
 // API endpoints
-app.get('/',(req , res) =>{
-    res.send("API is working!!!!")
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+
+app.get("/", (req, res) => {
+  res.send("API Working");
 });
 
-app.listen(port , () => console.log("App started on port: " + port));
+app.listen(port, () => console.log(`Server started on PORT: ` + port));
